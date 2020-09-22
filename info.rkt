9eb10f1c4c6a4d749f380a13076b805a474e6c84
emsenn <emsenn@emsenn.net>
2020-09-22 10:24:37 -0400

    Fix version number

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 
 (define pkg-desc
   "Perform operations on things with qualities")
-(define version "0.1.0")
+(define version "0.5.0")
 (define pkg-authors '("emsenn"))
 (define scribblings '(("scribblings/qtops.scrbl" (multi-page))))
