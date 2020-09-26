b8578e083b8231844c7cec6d8d35171695425a7d
emsenn <emsenn@emsenn.net>
2020-09-26 11:38:35 -0400

    Add nth function to utilities/lists
    Apparently something like this isn't provided by racket? wild.
    
    Might end up integrating it into qualities, like (emsenn 'nth-log 20)?
    
    But! Hey, this is kind of notable: this is the first git commit with
    really one change to it, just like one is supposed to do!
    
    Woooow.
    
    (I'm trying to be better about using git. Maybe soon I'll try
    branches.)

--- utilities/lists.rkt
+++ utilities/lists.rkt
@@ -1,6 +1,13 @@
 #lang racket
 
-(provide random-element)
+(provide nth
+         random-element)
+
+(define (nth l c)
+  (cond
+    [(null? l) (raise-argument-error 'nth "index OOB")]
+    [(= c 0) (first l)]
+    [else (nth (rest l) (- c 1))]))
 
 (define (random-element given-list)
   (list-ref given-list (random (length given-list))))
